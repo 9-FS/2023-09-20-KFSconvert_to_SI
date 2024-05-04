694ecef150a0c112db7abfe9f17ed66d9a3f0995
9FS <pray4spam@googlemail.com>
2024-05-04 17:57:02 +0000

    updated copyright notice year 2023 to year 2024

--- KFSconvert_to_SI/KFSconvert_to_SI.py
+++ KFSconvert_to_SI/KFSconvert_to_SI.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+# Copyright (c) 2024 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
 LENGTH={        # m
     "ft": 0.3048,
     "in": 0.0254,
